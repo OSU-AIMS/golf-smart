9868f88c4facf413d15a9319251a4f4af1c624f3
acbuynak <acbuynak@gmail.com>
2022-01-14 00:06:37 -0500

    first commit

---
+++ scripts/golf_swing.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#
+# Software License Agreement (Apache 2.0 License)
+# Copyright (c) 2021, The Ohio State University
+# The Artificially Intelligent Manufacturing Systems Lab (AIMS)
+#
+# Author: acbuynak
+
+
+import rospy
+
+
+def main():
+    rospy.loginfo("Hello World!")
+
+
+if __name__ == '__main__':
+    main()
